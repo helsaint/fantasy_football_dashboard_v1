878fb57548b77db1941c02499746ae5733d8cece
helsaint <alexei.ramotar@gmail.com>
2026-01-28 12:55:24 -0500

    Created a new player overview where we can search specific players and get some insights on them.

--- views/player_overview.py
+++ views/player_overview.py
@@ -4,9 +4,154 @@ import numpy as np
 from datetime import datetime
 import plotly.graph_objects as go
 import plotly.express as px
+from utils.load_fpl_features import load_player_data
 
 def show(filtered_df):
     st.header("Player Performance Overview")
+
+    if 'fetched_fpl_data' not in st.session_state:
+        st.session_state.fetched_fpl_data = None
+    if 'filtered_player_data' not in st.session_state:
+        st.session_state.filtered_player_data = None
+
+    df_fpl_features = load_player_data()
+    if st.session_state.fetched_fpl_data is None:
+        st.session_state.fetched_fpl_data = df_fpl_features
+
+    st.subheader("HTB Target Database")
+    # 1. Create a dictionary to map ID -> Display Label
+    # This makes lookups instant and clean
+    player_options = st.session_state.fetched_fpl_data.set_index('player_id').apply(
+        lambda x: f"{x['player_name']} ({x['team']})", axis=1
+        ).to_dict()
+    # Use selectbox as a search bar
+    # index=None ensures it starts empty
+    # 2. The Search Bar
+    selected_id = st.selectbox(
+        "Search for a player:",
+        options=player_options.keys(), # This stores the player_id
+        format_func=lambda x: player_options[x], # This shows "Name (Team)"
+        index=None,
+        placeholder="Type a name (e.g., 'Slayer')..."
+        )
+    # 3. Action based on selection
+    if selected_id:
+        # Pull the specific row for the unique ID
+        player_data = st.session_state.fetched_fpl_data[
+            st.session_state.fetched_fpl_data['player_id'] == selected_id].reset_index(drop=True)
+        st.session_state.filtered_player_data = player_data
+
+        st.divider()
+        col1, col2 = st.columns(2)
+        with col1:
+            st.metric("Player Name", player_data.loc[0,'player_name'])
+            st.caption(f"ID: {selected_id}")
+        with col2:
+            st.metric("Team", player_data.loc[0,'team'])
+            st.write(f"**Team:** {player_data.loc[0,'team']}")
+
+    if (
+        st.session_state.filtered_player_data is not None
+        ):
+        col1, col2, col3, col4 = st.columns(4)
+        player_data = st.session_state.filtered_player_data.copy()
+    
+        with col1:
+            st.metric("Total Points", 
+                      f"{st.session_state.filtered_player_data['total_points'].sum():,}")
+        with col2:
+            st.metric("Goals Scored", 
+                      f"{st.session_state.filtered_player_data['goals_scored'].sum():,}")
+        with col3:
+            st.metric("Assists", 
+                      f"{st.session_state.filtered_player_data['assists'].sum():,}")
+        with col4:
+            st.metric("Games over 60 mins", 
+                      f"{st.session_state.filtered_player_data['played_60'].sum():,}")
+        
+        st.markdown("---")
+        st.subheader("Residual Performance Analysis")
+        player_data['goals_assists'] = player_data['goals_scored'] + player_data['assists']
+        
+        fig_rpa = go.Figure()
+        fig_rpa.add_trace(
+            go.Scatter(
+                x=player_data['gw'],
+                y=player_data['goals_assists'],
+                mode='markers+lines',
+                name='Goals/Assists',
+            )
+        )
+        fig_rpa.add_trace(
+            go.Scatter(
+                x=player_data['gw'],
+                y=player_data['expected_goal_involvements'],
+                mode='markers+lines',
+                name='Expected Goal Involvement',
+            )
+        )
+
+        st.plotly_chart(fig_rpa, width='stretch', key="rpa_chart")
+        st.write("This is mainly used for attacking players")
+        st.write("- The Overperformer: If we see a player's points being consistently" \
+        "above that of the expected goals involvement then this player is overperforming." \
+        " The player is returning more points than the quality of their chances suggests." \
+        " This indicates a strong finishing ability. If the player hasn't been consistent" \
+        " like Haaland then expect a regression to the mean. The streak may be unsustainable" \
+        " and you should consider transferring out the player as soon as there is a dip in" \
+        " form")
+        st.write("- The Underperfromer: This is shown by the expected goals and assists" \
+        " line being consistently above the actual goals+assits lines. This indicates that" \
+        " the player is getting into good positions but is not scoring. If this is unusual" \
+        " behavior expect the player to eventually 'come good' and they are a candidate" \
+        " as a differential buy.")
+        st.write("- The Reliable Asset: The player is scoring the points we expect them" \
+        " they are consistent and we can count on their points.")
+
+        st.markdown("---")
+        st.subheader("Noise vs Signal")
+
+        fig_nsv = go.Figure()
+        fig_nsv.add_trace(
+            go.Scatter(
+                x=player_data['gw'],
+                y=player_data['total_points'],
+                mode='markers+lines',
+                name='Total Points',
+            )
+        )
+        fig_nsv.add_trace(
+            go.Scatter(
+                x=player_data['gw'],
+                y=player_data['pts_ewma'],
+                mode='markers+lines',
+                name='Weighted Moving Average',
+            )
+        )
+        fig_nsv.add_trace(
+            go.Scatter(
+                x=player_data['gw'],
+                y=player_data['pts_roll_5'],
+                mode='markers+lines',
+                name='Rolling 5',
+            )
+        )
+
+        st.plotly_chart(fig_nsv, width='stretch', key="nsv_chart")
+        st.write(" We use Total Points to see the ceiling, the Rolling 5 to capture the" \
+        " current player's current form, and the EWMA to provide a weighted baseline that" \
+        " filters out the chaos of a single gameweek.")
+        st.write("- The Breakout: the Rolling 5 crosses above the Weighted Moving Average" \
+        " The player is in a hot streak. Their current form is better than their season" \
+        " average.")
+        st.write("- The Slump: the Rolling 5 crosses below the Weighted Moving Average." \
+        " The player's form has dropped below their baseline. They may be a good candidate" \
+        " to bench.")
+        st.write("- The Trap: the weighted moving average is steady but we see actual points" \
+        " spikes. Don't chase this player unless their Weighted Moving Average is also" \
+        " increasing.")
+
+
     
     # Top row: Key metrics
     col1, col2, col3, col4 = st.columns(4)
@@ -26,7 +171,6 @@ def show(filtered_df):
         avg_points = filtered_df['total_points'].mean()
         st.metric("Avg Points/GW", f"{avg_points:.1f}")
     
-    st.markdown("---")
     
     # Middle row: Two charts
     col1, col2 = st.columns(2)
